08e829fb6dab15918ab41440c66b9e5030c91320
郑康霖 <1255964530@qq.com>
2018-12-05 16:49:17 +0800

    TOdoList组件改为无状态组件，只负责渲染视图

--- src/TodoListUI.js
+++ src/TodoListUI.js
@@ -1,6 +1,30 @@
 import React, { Component, Fragment } from 'react'
 import { Input, Button, List } from 'antd'
 
+// 无状态组件，只有props属性，一般只用来显示视图，性能好
+const TodoListUI = (props) => {
+  return (
+    <Fragment>
+      <div>
+        <Input 
+        style={{ marginRight: 5, marginTop: 10, marginLeft: 10, width: 300}}
+        type="text"
+        value={props.inputValue}
+        onChange={props.handleInputChange} />
+        <Button type="primary" onClick={props.handleAddItem}>添加</Button>
+      </div>
+      <List
+        style={{marginLeft: 10, marginTop: 10, width: 300}}
+        bordered
+        dataSource={props.list}
+        renderItem={(item, index) => (<List.Item onClick={() => props.handleDelItem(index)}>{item}</List.Item>)}>
+      </List>
+    </Fragment>
+  )
+}
+
+// 有状态组件，同时有state和props，一般包含逻辑的处理
+/*
 class TodoListUI extends Component {
   constructor(props) {
     super(props)
@@ -27,5 +51,5 @@ class TodoListUI extends Component {
     )
   }
 }
-
+*/
 export default TodoListUI

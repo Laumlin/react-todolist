6cb57e211d316aee5ac1e8c953ccbffa286c44ef
郑康霖 <1255964530@qq.com>
2018-12-05 14:41:55 +0800

    实现todolist的添加功能

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class TodoList extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleAddItem = this.handleAddItem.bind(this)
   }
 
   render() {
     return (
-      <div>hello</div>
+      <Fragment>
+        <div>
+          <input 
+          type="text"
+          value={this.state.inputValue}
+          onChange={this.handleInputChange} />
+          <button onClick={this.handleAddItem}>添加</button>
+        </div>
+        <ul>
+          {this.state.list.map((item, index) => (<li key={index}>{item}</li>))}
+        </ul>
+      </Fragment>
     )
+  }
+
+  handleInputChange(e) {
+    const inputValue = e.target.value
+    this.setState(() => {
+      return {inputValue}
+    })
+  }
+
+  handleAddItem() {
+    this.setState((prevState) => {
+      const newState = prevState
+      const inputValue = newState.inputValue
+      newState.list.push(inputValue)
+      return {newState}
+    })
   }
 }
 

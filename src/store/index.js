770766d0cb18fe21ec4c71cebd5af0cdef015ec4
郑康霖 <1255964530@qq.com>
2018-12-06 11:15:50 +0800

    配置redux-saga中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from './reducer'
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
+import todoList from  './sagas.js'
+// import thunk from 'redux-thunk'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const sagaMiddleware = createSagaMiddleware()
 const enhancer = composeEnhancers(
-  applyMiddleware(thunk),
+  // applyMiddleware(thunk),
+  applyMiddleware(sagaMiddleware)
 )
 const store = createStore(
   reducer,
   enhancer
 )
+sagaMiddleware.run(todoList)
 
 export default store
